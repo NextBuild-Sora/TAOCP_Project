b2c957b95d2f0dda1b4ca4eb343417f475d13df0
字母表 <42495830+End404@users.noreply.github.com>
2022-01-22 18:18:49 +0800

    Add files via upload

---
+++ 附录C4_Django_模块_models/Django_demo_C4/migrate.py
@@ -0,0 +1,21 @@
+
+
+import os 
+
+
+# os.system( "python manage.py makemigrations" )
+# os.system( "python manage.py migrate" )
+
+os.system("python D:/ProgramData/PyCharm_Community_Edition/"
+    "PyCharm-Community-2020/Python网络爬虫程序技术/"
+        "附录C4_Django_模块_models/Django_demo_C4/manage.py makemigrations")
+
+os.system("python D:/ProgramData/PyCharm_Community_Edition/"
+    "PyCharm-Community-2020/Python网络爬虫程序技术/"
+        "附录C4_Django_模块_models/Django_demo_C4/manage.py migrate")
+
+
+
+
+
+
---
+++ 附录C4_Django_模块_models/Django_demo_C4/runServer.py
@@ -0,0 +1,14 @@
+
+
+import os 
+
+
+# os.system( "python manage.py runserver" )
+
+os.system("python D:/ProgramData/PyCharm_Community_Edition/"
+    "PyCharm-Community-2020/Python网络爬虫程序技术/"
+        "附录C4_Django_模块_models/Django_demo_C4/manage.py runserver")
+
+
+
+
