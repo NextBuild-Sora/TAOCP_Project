f01de8c3f34d574494e65bd784b77e115a8b937e
字母表 <42495830+End404@users.noreply.github.com>
2022-01-22 18:09:46 +0800

    Add files via upload

---
+++ 附录C2_Django-模版-templates/Django_demo_C2/runServer.py
@@ -0,0 +1,8 @@
+
+import os 
+
+# os.system("python manage.py runserver")
+os.system("python D:/ProgramData/PyCharm_Community_Edition/"
+    "PyCharm-Community-2020/Python网络爬虫程序技术/"
+        "附录C2_Django-模版-templates/Django_demo_C2/manage.py runserver")
+
