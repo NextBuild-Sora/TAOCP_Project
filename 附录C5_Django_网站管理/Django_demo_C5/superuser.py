b7b8852f65a5667ac9cf1f1e6e74d40107719126
字母表 <42495830+End404@users.noreply.github.com>
2022-01-22 18:24:23 +0800

    Add files via upload

---
+++ 附录C5_Django_网站管理/Django_demo_C5/superuser.py
@@ -0,0 +1,13 @@
+
+#创建超级用户
+
+import os 
+
+
+# os.system( "python manage.py createsuperuser" )
+
+
+os.system("python D:/ProgramData/PyCharm_Community_Edition/"
+    "PyCharm-Community-2020/Python网络爬虫程序技术/"
+        "附录C5_Django_网站管理/Django_demo_C5/manage.py createsuperuser" )
+
